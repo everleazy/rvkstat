47b88ccd2ae7b006f6422b209ff57987e5397971
everleazy <39529705+everleazy@users.noreply.github.com>
2020-09-12 23:38:35 +0300

    Update api_version_checker.R

--- R/api_version_checker.R
+++ R/api_version_checker.R
@@ -1,5 +1,5 @@
 api_version_checker <- function(v){
-  current_version <- "5.73"
+  current_version <- "5.122"
   
   if(is.null(v)) return(current_version)
   if(as.numeric(v) < 4) return(current_version)
