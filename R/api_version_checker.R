c08f6a99c8d83af0e61c4be3181daaae9a1c0c48
everleazy <39529705+everleazy@users.noreply.github.com>
2020-12-24 10:41:28 +0300

    Update api_version_checker.R

--- R/api_version_checker.R
+++ R/api_version_checker.R
@@ -1,10 +1,5 @@
  api_version_checker <- function(v){
- library(rvest)
-  
-  url <- "https://vk.com/dev/versions"
-  data <- read_html (url)
-  apiV <- html_nodes(data, css = ".dev_version_num.fl_l")
-  apiV <- html_text(apiV[1])
+  apiV <- "5.122"
   ifelse(is.null(apiV), "5.122",apiV)
   
   
